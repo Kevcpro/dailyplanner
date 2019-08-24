2aac6c197242d4694d011fdabe52a13ea38d924b
Kevin Cho <kevc.pro@gmail.com>
2019-08-24 09:58:05 -0400

    Added Calendar to Homepage

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -12,6 +12,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import Calendar from 'containers/Calendar';
 
 import GlobalStyle from '../../global-styles';
 
@@ -19,7 +20,8 @@ export default function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/" component={Calendar} />
+        <Route exact path="/homepage" component={HomePage} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
